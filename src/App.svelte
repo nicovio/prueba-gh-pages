76e2ab4f562768a77400f6007ce30bed382f1785
nicovio <nicolasviotti90@gmail.com>
2021-02-21 05:57:48 -0300

    get player

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,42 @@
 <script>
-	export let name;
+  import { onMount } from "svelte";
+
+  let jugador;
+
+  async function getPlayer(nombre, apellido) {
+    jugador = await fetch(
+      `https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?p=${nombre}%20${apellido}`
+    );
+  }
+
+  onMount(getPlayer("lionel", "messi"));
 </script>
 
 <main>
-	<h1>Hola {name}!</h1>
-	<h2>a ver que pasa</h2>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  {#if jugador}
+    <h1>Nombre: {jugador.strPlayer}</h1>
+    <h1>Equipo actual: {jugador.strTeam}</h1>
+  {/if}
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
